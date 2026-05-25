d9edfb25c39afa9c6c0b112c3447c232e8267946
mirkg <mirkg@github>
2026-05-25 09:08:14 +0200

    Do not restart journal threads

--- plugin.py
+++ plugin.py
@@ -1,5 +1,5 @@
 """
-<plugin key="LogMonitor" name="LogMonitor" version="0.1.2">
+<plugin key="LogMonitor" name="LogMonitor" version="0.1.3">
     <description>
         <h2>LogMonitor Plugin</h2>
         <p>Plugin to monitor logs streams for specific patterns.</p>
